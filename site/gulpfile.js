0e4e4f4c6ac958c130c6204197ff9a46eda20e8d
Caio Batista <cbatista.93@gmail.com>
2017-07-02 20:08:42 -0300

    teste

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -9,6 +9,7 @@ var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
 // Set the banner content
+// Set the banner content 2
 var banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
